39ae538d3122221bb3c8d83c89afa8d396d748a8
Apollo Tang <tang.apollo@gmail.com>
2017-06-29 21:53:49 -0400

    [master] typography

--- src/main.js
+++ src/main.js
@@ -4,6 +4,8 @@ import {render} from 'react-dom';
 import style from 'main.less';
 import fonts from 'common/fonts';
 
-import FontTest from './font-test';
+import FontTest from 'modules/font-test';
+import Typography from 'modules/typography-test';
+
 const rootContainer = document.getElementById("root-container");
-render( <FontTest />, rootContainer);
+render( <Typography />, rootContainer);
